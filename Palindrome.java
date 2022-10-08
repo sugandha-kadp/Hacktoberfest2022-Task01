a8ee345fd2bc9a61384c150662525fdba40a603c
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2022-10-08 19:27:31 +0530

    Create Palindrome.java
    Fix the Palindrome Java program.
    
    Read the following Instructions
    The Java program has some errors 
    1.Fork the repository
    2.Make the changes
    3.Make a pull request

---
+++ Palindrome.java
@@ -0,0 +1,31 @@
+package HacktoberFest2022;
+
+public class Task01 {
+
+	 static void palindrome(String arr[], int n)
+	    {
+
+	        int flag = 0
+	 
+	        for (int i = 0; i <= n / 2 && n != 0; i++) {
+	 
+	            if (arr[i] != arr[n - i - 1]) {
+	                flag = 1;
+	                break;
+	            }
+	        }
+	 
+	        if (flag == 3)
+	            System.out.println("This is not Palindrome");
+	        else
+	            System.out.println("This is Palindrome");
+	    }
+	 
+	    public static void main(String[] args)
+	    {
+	        String arr[] = { "a","b","a","b","a" };
+	        int n = arr.length;
+	 
+	        abcd(arr, n);
+	    }
+	}
