6e214a90e2e3ee53e788decf88fe2513bab8d820
uththaraHansamali <92640862+uththaraHansamali@users.noreply.github.com>
2022-10-12 10:13:50 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,19 +1,14 @@
 package HacktoberFest2022;
-
-public class TEST1 {
+public class Task01 {
 	 static void palindrome(String arr[], int n)
 	    {
-
 	        int flag = 0;
-	 
 	        for (int i = 0; i <= n / 2 && n != 0; i++) {
-	 
 	            if (arr[i] != arr[n - i - 1]) {
 	                flag = 1;
 	                break;
 	            }
 	        }
-	 
 	        if (flag == 1)
 	            System.out.println("This is not Palindrome");
 	        else
@@ -24,7 +19,6 @@ public class TEST1 {
 	    {
 	        String arr[] = { "a","b","a","b","a" };
 	        int n = arr.length;
-	 
 	        palindrome(arr, n);
 	    }
 	}
